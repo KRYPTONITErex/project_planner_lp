7fb5094a51b6abe71f3891c20fd3dcacdd708476
Lwin Phyo <lwinphyo271395@gmail.com>
2025-01-14 07:34:18 +0700

    ready to practice

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,9 @@
 <template>
-    <div class="home">
-        <h1>Home</h1>
-
-        <div class="projects" v-for="project in projects" :key="project.id">
-            <SingleProject :project="project"></SingleProject>
-        </div>
-    </div>
-  
+    <h1>Home</h1>
 </template>
 
 <script>
-import SingleProject from '../components/SingleProject.vue'
 export default {
-    data() {
-        return {
-            projects: []
-        }
-    },
-    name: 'Home',
-    components: {
-    SingleProject,
-        
-    },
-    mounted() {
-        fetch('http://localhost:3000/projects/')
-        .then((res)=>{
-            return res.json()
-        })
-        .then((data)=>{
-            // console.log(data)
-            this.projects = data
-        })
-    }
 
 }
 </script>
